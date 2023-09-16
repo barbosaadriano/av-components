95e46b50dc7eea6037b2223e25a2828832c52d81
barbosaadriano <adriano@adrianob.com.br>
2023-09-16 14:39:58 -0300

    more inproviments

--- src/components/Selects/AvForeignKeySelect.vue
+++ src/components/Selects/AvForeignKeySelect.vue
@@ -1,30 +1,52 @@
 <template>
-    <div class="dropdown">
-        <label class="form-label">{{ label }}</label><br>
+    <div class="fk-wrapper">
+        <label class="form-label mb-2">{{ label }}</label><br>
         <input type="hidden" :name="props.name" :value="joinedSelectedKeys">
-        <input class="form-control dropdown-toggle" type="text" data-bs-toggle="dropdown" :value="joinedSelectedValues"
-            readonly>
-        <ul class="dropdown-menu"><!--wrapper para pesquisa-->
-            <div class="card">
-                <div class="card-header"><!--header da pesquisa-->
-                    <input type="search" class="form-control">
-                    <div>
-                        Select all, number of pages, navigations clear selection
-                    </div>
-                </div>
-                <div class="card-body"><!--body to list itens-->
+        <div class="input-group">
+            <input class="form-control" type="text" :value="joinedSelectedValues" readonly>
+            <button class="btn btn-secondary" @click="edit">
+                <font-awesome-icon icon="fa-solid fa-pen-to-square"></font-awesome-icon>
+            </button>
+        </div>
 
-                    <li class="dropdown-item" v-for="item in availables" :key="item[keyFieldName]">
-                        {{ item[valueFildName] }}
-                    </li>
-
-                </div>
-                <div class="card-footer">
-                    footer goes here,
-                    close button, apply button
+        <div class="searcher" :class="{hidded:!editing}">
+            <div class="searcher-header"><!--header da pesquisa-->
+                <input type="search" ref="searchf" placeholder="localizar" class="form-control">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <span>
+                        Exibindo 10 de 1000 registros
+                    </span>
+                    <button type="button" class="btn me-md-2 btn-sm btn-secondary" @click="closeEdit">
+                        <font-awesome-icon icon="fa-solid fa-square-check"></font-awesome-icon> &nbsp; selecionar todos
+                    </button>
+                    
+                    <button type="button" class="btn me-md-2 btn-sm btn-secondary" @click="closeEdit">
+                        <font-awesome-icon icon="fa-solid fa-broom"></font-awesome-icon> &nbsp; limpar
+                    </button>
+                    
+                    <button type="button" class="btn me-md-2 btn-sm btn-danger" @click="closeEdit">
+                        <font-awesome-icon icon="fa-solid fa-xmark"></font-awesome-icon> &nbsp; fechar
+                    </button>
                 </div>
             </div>
-        </ul>
+            <div class="searcher-body"><!--body to list itens-->
+
+                <div v-for="item in availables" :key="item[keyFieldName]">
+                    {{ item[valueFildName] }}
+                </div>
+
+            </div>
+            <div class="searcher-footer">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">                    
+                    <button type="button" class="btn me-md-2 btn-sm btn-secondary" @click="closeEdit">
+                        <font-awesome-icon icon="fa-solid fa-ban"></font-awesome-icon> &nbsp; cancelar
+                    </button>
+                    <button type="button" class="btn me-md-2 btn-sm btn-primary" @click="closeEdit">
+                        <font-awesome-icon icon="fa-solid fa-check"></font-awesome-icon> &nbsp; confirmar
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -62,6 +84,53 @@ const joinedSelectedValues = computed(() => {
     return props.selecteds.map((si) => si[props.valueFildName]).join(",");
 });
 
+const editing = ref(false);
+const edit = ()=> {
+    editing.value = true;
+    setTimeout(()=>{
+        searchf.value.focus();
+    },100);
+};
+const closeEdit = ()=> {
+    editing.value = false;
+};
+
+const searchf = ref(null);
 // selected itens
 // available itens
 </script>
+<style scoped>
+.searcher {
+    background: #ffffff;
+    box-sizing: border-box;
+    position: relative;
+    top: -45px;
+    z-index: 10;
+    border-radius: .5rem;
+    border: 1px solid transparent;
+    margin-top: 0.2rem;
+    padding: 0.5rem;
+    box-shadow: 1px 1px 5px lightgray;
+    /* display: none; */
+}
+.hidded {
+    display: none;
+}
+
+.searcher .form-control {
+    margin: 0px;
+    border: 1px solid #eeeeee;
+    border-radius: .01rem;
+    margin-bottom: .5rem;
+}
+
+.searcher-header,
+.searcher-body {
+    margin-bottom: .5rem;
+}
+.searcher-body {
+    height: 200px;
+    overflow: hidden;
+    overflow-y: scroll;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import AvComponents from '.'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faPenToSquare, faXmark, faSquareCheck, faBroom, faCheck, faBan } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPenToSquare)
+library.add(faXmark)
+library.add(faSquareCheck)
+library.add(faBroom)
+library.add(faCheck)
+library.add(faBan)
+
 import './scss/style.scss'
 import { Popover } from 'bootstrap'
 
 createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
 .use(AvComponents)
 .mount('#app')
 
